4ed21b0004546b299544984bab6fca254159ba11
amirhoseinensafi <amirmahdion1@gmail.com>
2024-12-09 14:26:40 +0330

    start coding the infrastructure

--- src/domain/infrastructure/user/userBroker.ts
+++ src/domain/infrastructure/user/userBroker.ts
@@ -1,15 +1,21 @@
+import { Consumer } from "kafkajs";
 import produser from "../../../kafka/produser";
 import UserConsumer from "./userConsumerMaker";
 
 class UserBroker{
 
-    private readonly userConsumer : UserConsumer
+    private topic : {[key : string] : string} = {}
+    private readonly userConsumer : UserConsumer ;
     private createUserMap : Map<string , string> ;
     private updateUserMap : Map<string , string> ;
     private deleteUserMap : Map<string , string> ;
     private readUserMap   : Map<string , string> ;
 
     constructor (userConsumer : UserConsumer) {
+        this.topic.create  = "createUserProcess"
+        this.topic.update  = "updateUserProcess"
+        this.topic.delete  = "deleteUserProcess"
+        this.topic.read    = "readUserProcess"
         this.userConsumer  = userConsumer
         this.createUserMap = new Map<string , string>()
         this.updateUserMap = new Map<string , string>()
@@ -17,45 +23,76 @@ class UserBroker{
         this.readUserMap   = new Map<string , string>()
     }
 
-    public create = async (userInfo : string , key : string) : Promise<string> => {
-        let result : string = await new Promise(async (resolve , reject) => {
-            
+    private sendAndRecive = async (dto : {consumer : Consumer , map : Map<string , string> , topic : string , userInfo : string , key : string}) : Promise<string> => {
+        let {consumer , map , topic , userInfo , key} = dto 
+        let result : string = await new Promise( async (resolve , reject) => {
             await produser.send({
-                topic : "createUserProcess" ,
-                messages : [{value :  userInfo , key : key}]
+                topic : topic ,
+                messages : [{ value : userInfo , key : key }]
             })
-            
+
             const timeout = setTimeout(() => {
-                let result = this.createUserMap.get(key)
-                if(result){
+                let result = map.get(key)
+                if(result) {
                     resolve(result)
-                } 
-                else {
+                }else{
                     reject("time end")
                 }
+            } , 500)
 
-            }, 500);
-
-            let result = this.createUserMap.get("") 
-            if(result){
-                resolve(result)
-            }
-            
-            await this.userConsumer.createUserConsumer.run({
-                eachMessage : async ({topic , partition , message}) => {
-
+            await consumer.run({
+                eachMessage : async ({message}) => {
+    
                     let messageKey   = message.key?.toString()   as string
                     let messageValue = message.value?.toString() as string
-
-                    if(messageKey === key) {
+    
+                    if(messageKey === key){
+                        clearTimeout(timeout)
                         resolve(messageValue)
                     }else{
-                        this.createUserMap.set(messageKey , messageValue)
+                        map.set(messageKey , messageValue)
                     }
                 }
             })
         })
 
         return result;
+    }
+
+    public create = async (userInfo : string , key : string) : Promise<string> => {
+
+        let informationToSend = {consumer : this.userConsumer.createUserConsumer , map : this.createUserMap , topic : this.topic.create , userInfo , key}
+
+        let result = await this.sendAndRecive(informationToSend)
+
+        return result
+    }
+
+    public update = async (userInfo : string , key : string) : Promise<string> => {
+
+        let informationToSend = {consumer : this.userConsumer.updateUserConsumer , map : this.updateUserMap , topic : this.topic.read , userInfo , key}
+
+        let result = await this.sendAndRecive(informationToSend)
+        
+        return result
+    }
+
+    public delete = async (userInfo : string , key : string) => {
+
+        let informationToSend = {consumer : this.userConsumer.deleteUserConsumer , map : this.deleteUserMap , topic : this.topic.delete , userInfo , key}
+
+        let result = await this.sendAndRecive(informationToSend)
+        
+        return result
+    }
+
+    public read = async (userInfo : string , key : string) => {
+
+        let informationToSend = {consumer : this.userConsumer.readUserConsumer , map : this.readUserMap , topic : this.topic.read , userInfo , key}
+
+        let result = await this.sendAndRecive(informationToSend)
+        
+        return result
+
     }
 }
